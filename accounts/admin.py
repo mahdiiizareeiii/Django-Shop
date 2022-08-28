52ee962654854e2bfa308fa830168e376713550b
Mahdi <mahdi.zareei.80@gmail.com>
2022-08-28 10:20:29 +0430

    Add Some Features

--- accounts/admin.py
+++ accounts/admin.py
@@ -12,12 +12,14 @@ class OtpCodeAdmin(admin.ModelAdmin):
 class UserAdmin(BaseUserAdmin):
     form = UserChangeForm
     add_form = UserCreationForm
-    list_display = ("email", "phone_number", "is_admin")
+
+    list_display = {"email", "phone_number", "is_admin"}
     list_filter = ("is_admin",)
+    readonly_fields = ("last_login",)
 
     fieldsets = (
         (None, {"fields":("email", "phone_number", "full_name", "password")}),
-        ("permissions", {"fields":("is_active", "is_admin", "last_login")}),
+        ("permissions", {"fields":("is_active", "is_admin", "is_superuser", "last_login", "groups", "user_permissions")}),
     )
 
     add_fieldsets = (
@@ -26,7 +28,15 @@ class UserAdmin(BaseUserAdmin):
 
     search_fields = ("email", "full_name")
     ordering = ("full_name",)
-    filter_horizontal = ()
+    filter_horizontal = ("groups", "user_permissions")
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+        if not is_superuser:
+            form.base_fields["is_superuser"].disabled = True
+        return form
+
 
 admin.site.unregister(Group)
 admin.site.register(User, UserAdmin)
--- orders/cart.py
+++ orders/cart.py
@@ -20,6 +20,10 @@ class Cart:
             item["total_price"] = int(item["price"]) * item["quantity"]
             yield item    
 
+    def __len__(self):
+        return sum(item["quantity"] for item in self.cart.values())
+
+
 
     def add(self, product, quantity):
         product_id = str(product.id)
@@ -39,3 +43,7 @@ class Cart:
 
     def get_total_price(self):
         return sum(int(item["price"]) * item["quantity"] for item in self.cart.values())
+
+    def clear(self):
+        del self.session[CART_SESSION_ID]
+        self.save()
